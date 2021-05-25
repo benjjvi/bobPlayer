b9eb4b0c2cc691ce5ebc788cde6a8e39960a13b8
ttvfimin <ben.voisey2@gmail.com>
2021-05-25 21:36:43 +0100

    added json for all files index.

---
+++ dikke_fiets.py
@@ -0,0 +1,31 @@
+#file name is a joke with dutch friend luuk. dikke fiets = fat bikes
+
+import glob
+
+
+fat_bikes = {}
+
+ep_num = 1
+
+for i in range(1,32):
+    a = glob.glob(r"A:\BobRoss\S" + str(i) + r"\*.ogv")
+
+    for x in a:
+        x = x[11:]
+
+        xcomp = ""
+        xsplit = list(x)
+
+        for item in xsplit:
+            if item == "\\": 
+                item = "/"
+            xcomp = xcomp + str(item)
+
+        x = "http://bildsben.hopto.org:8080/" + xcomp
+
+        print(x)
+
+        fat_bikes[str(ep_num)] = x
+        ep_num += 1
+
+open("items.dict.json", "w").write(str(fat_bikes))
