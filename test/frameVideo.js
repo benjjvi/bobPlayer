d7730a9f752d9539c3c565126bbbc697db67459c
ttvfimin <ben.voisey2@gmail.com>
2021-05-28 13:44:05 +0100

    updateeeeeeeee

---
+++ test/frameVideo.js
@@ -0,0 +1,27 @@
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+if (urlParams.has("episode")) {
+    var episode = urlParams.get('episode')
+}
+else {
+    var episode = 1
+}
+
+console.log("finding episode and framing it")
+var link = items[episode]
+console.log(link)
+
+var videoreplaced = '<source src="' + link + '" type="video/ogg">'
+
+console.log(document.getElementById("replacevideo").outerHTML)
+
+document.getElementById("replacevideo").outerHTML = videoreplaced;
+
+
+var nextEpisode = parseInt(episode) + 1
+
+var buttonreplaced = '<button class="transparent"><a href="./testing?episode=' +nextEpisode+'">Next Video</a></button>' 
+
+document.getElementById("next__video").outerHTML = buttonreplaced;
